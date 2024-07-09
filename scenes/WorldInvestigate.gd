5a00b4d01301d9325583602e639af0593e1a9739
NxmNile <laksikapanich@gmail.com>
2024-07-09 12:41:12 +0900

    popup ui

--- scenes/WorldInvestigate.gd
+++ scenes/WorldInvestigate.gd
@@ -1,6 +1,8 @@
 extends Area3D
 
 @onready var Character = $"../Character"
+var object_name : String
+var is_in_collider : bool
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,4 +10,22 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	transform = Character.transform
 	pass
+
+
+func _on_area_entered(area):
+	print(area.name)
+	object_name = area.name
+	is_in_collider = true
+	$"../Camera3D/Control/InvestigatePanel".visible = true
+	if object_name=="Door":
+		$"../Camera3D/Control/InvestigatePanel/InvestigateText".text = "Enter"
+	else:
+		$"../Camera3D/Control/InvestigatePanel/InvestigateText".text = "Investigate"
+	pass # Replace with function body.
+
+
+func _on_area_exited(area):
+	$"../Camera3D/Control/InvestigatePanel".visible = false
+	pass # Replace with function body.
