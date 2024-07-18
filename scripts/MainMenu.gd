d85582b58b716ea6db79218a2f9d89604afe963c
NxmNile <laksikapanich@gmail.com>
2024-07-18 13:29:52 +0900

    New Main Menu

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -1,29 +1,33 @@
 extends Node
 const menu = preload("res://scenes/main_menu.tscn")
 const intro = preload("res://scenes/intro.tscn")
+
 func _ready():
 	Keepdata.scene_name = "Menu"
-	$PlayButton/PlayButtonBG/Label.text = tr("StartButton")
-	$ExitButton/ExitButtonBG/Label.text = tr("ExitButton")
-func _on_exit_button_pressed():
-	get_tree().quit()
-	pass
+	$StartButton/Label.text = tr("StartButton")
+	$Exit/Label.text = tr("ExitButton")
 
-func _on_play_button_pressed():
-	Keepdata.scene_name = "Intro"
-	menu.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE).queue_free()
-	get_tree().change_scene_to_packed(intro)
-	pass
 
 func _on_setting_button_pressed():
 	$SettingPanel.visible = true
 
 func _change_to_english():
 	TranslationServer.set_locale("en")
-	$PlayButton/PlayButtonBG/Label.text = tr("StartButton")
-	$ExitButton/ExitButtonBG/Label.text = tr("ExitButton")
+	$StartButton/Label.text = tr("StartButton")
+	$Exit/Label.text = tr("ExitButton")
 
 func _change_to_japanese():
 	TranslationServer.set_locale("jp")
-	$PlayButton/PlayButtonBG/Label.text = tr("StartButton")
-	$ExitButton/ExitButtonBG/Label.text = tr("ExitButton")
+	$StartButton/Label.text = tr("StartButton")
+	$Exit/Label.text = tr("ExitButton")
+
+
+func _on_start_button_pressed():
+	Keepdata.scene_name = "Intro"
+	menu.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE).queue_free()
+	get_tree().change_scene_to_packed(intro)
+
+
+func _on_exit_pressed():
+	get_tree().quit()
+
